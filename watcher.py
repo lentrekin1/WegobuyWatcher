a9f1b4ef5972327d8133503223c2b05dcdfb7d26
lance <lancee478@gmail.com>
2021-03-27 21:02:48 -0700

    added downloading data from S3 and adding to local file

--- watcher.py
+++ watcher.py
@@ -6,6 +6,7 @@ import time
 import logging
 import sys
 import boto3
+from botocore.exceptions import ClientError
 from datetime import datetime
 
 time_delay = 5
@@ -35,6 +36,7 @@ key = os.environ.get('AWS_ACCESS_KEY_ID')
 secret = os.environ.get('AWS_SECRET_ACCESS_KEY')
 s3 = boto3.client('s3')
 upload_time = 10 * 60
+tmp_csv_name = 'tmp_csv'
 
 on_heroku = True if os.environ.get('on_heroku') == 'True' else False
 
@@ -54,23 +56,52 @@ def upload():
     except:
         logger.exception(f'Upload of {upload_log_file} to S3 bucket {bucket} failed')
 
+def download():
+    try:
+        with open(tmp_csv_name, 'wb') as f:
+            s3.download_fileobj(bucket, data_file if on_heroku else 'data-local.csv', f)
+        logger.info(f'Downloaded {data_file if on_heroku else "data-local.csv"} from S3 bucket {bucket} to file {tmp_csv_name}')
+        return True
+    except ClientError:
+        logger.info(f'File {data_file if on_heroku else "data-local.csv"} not found on S3 bucket {bucket}')
+    except:
+        logger.exception(f'Error downloading file {data_file if on_heroku else "data-local.csv"} from S3 bucket {bucket}')
+
 def watch():
     try:
         last_upload = time.time()
         logger.info('**** STARTING WATCHER ****')
         logger.info(
             f'Program settings: time_delay: {time_delay}, data_file: {data_file}, num_items: {num_items}, log_file: {log_file}')
+        old_ids = []
+        old_exists = download()
         if not os.path.isfile(data_file):
             with open(data_file, 'w', newline='', encoding='utf-8') as f:
                 writer = csv.writer(f)
                 writer.writerow(row_names)
                 logger.info(f'Data file {data_file} not found, created it with row names {row_names}')
-        old_ids = []
+
         with open(data_file, 'r', encoding='utf-8') as f:
             reader = csv.DictReader(f)
             for row in reader:
                 old_ids.append(row['id'])
             logger.info(f'Read {len(old_ids)} ids from {data_file}')
+
+        if old_exists:
+            with open(tmp_csv_name, 'r', encoding='utf-8') as f:
+                reader = csv.DictReader(f)
+                with open(data_file, 'a', newline='', encoding='utf-8') as f:
+                    writer = csv.DictWriter(f, fieldnames=row_names)
+                    num_added = 0
+                    num_old = 0
+                    for row in reader:
+                        num_old += 1
+                        if row['id'] not in old_ids:
+                            writer.writerow(row)
+                            old_ids.append(row['id'])
+                            num_added += 1
+            logger.info(f'Read {num_old} entries from old data {tmp_csv_name} and added {num_added} entries to {data_file}')
+
         logger.info('Starting main loop...')
         while True:
             response = requests.get(url + str(num_items))
@@ -87,8 +118,8 @@ def watch():
                     writer = csv.DictWriter(f, row_names)
                     writer.writerows(curr_data)
                     logger.info(f'Wrote {len(curr_data)} new items to {data_file}: {curr_data}')
-            else:
-                logger.info('No new items found')
+            #else:
+                #logger.info('No new items found')
             if time.time() - last_upload > upload_time:
                 upload()
                 last_upload = time.time()
@@ -97,6 +128,7 @@ def watch():
         logger.exception('ERROR')
 
 if __name__ == '__main__':
-    #watch()
-    upload()
+    watch()
+    #upload()
+
 
