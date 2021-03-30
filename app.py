e94ccb47af3ea7145536b0b6ae9cbee18862adbe
lance <lancee478@gmail.com>
2021-03-30 15:47:37 -0700

    broke website spreadsheet into pages

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
-from flask import Flask, render_template, Response
+from flask import Flask, render_template, Response, redirect
 import csv
 import os
 import threading
 import watcher
 
+page_len = 100
 watch_thread = threading.Thread(target=watcher.watch, args=())
 watch_thread.start()
 app = Flask(__name__)
 
-def get_data():
+def get_data(pg=None):
     if not os.path.isfile(watcher.data_file):
         with open(watcher.data_file, 'w', newline='', encoding='utf-8') as f:
             writer = csv.writer(f)
@@ -17,12 +18,27 @@ def get_data():
     with open(watcher.data_file, 'r', encoding='utf-8') as f:
         reader = csv.DictReader(f)
         for row in reader:
-            data.append(row)
+                data.append(row)
+    if pg:
+        data = data[(pg - 1) * page_len: pg * page_len]
     return list(reversed(data))
+
+def get_last_page():
+    data = get_data()
+    return int(len(data) / page_len) + 1
 
 @app.route('/')
 def show_data():
-    return render_template('home.html', data=get_data(), header=watcher.row_names)
+    return redirect('/page/' + str(get_last_page()))
+
+@app.route('/page/<int:pg>')
+def page(pg):
+    last_pg = get_last_page()
+    if pg > last_pg:
+        return redirect('/page/' + str(last_pg))
+    if pg < 1:
+        return redirect('/page/' + str(1))
+    return render_template('home.html', data=get_data(pg=int(pg)), isFirst='' if pg != 1 else 'none', isLast='' if pg != last_pg else 'none', totalNum=len(get_data()), header=watcher.row_names)
 
 @app.route("/getcsv")
 def give_data():
